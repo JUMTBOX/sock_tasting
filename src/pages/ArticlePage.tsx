e1355983c3e74a5ebe4a5f0e50754861eb58797b
JUMTBOX <yjey12@naver.com>
2023-08-29 22:44:35 +0900

    #2 feat. ArticlePage 데이터 바인딩 해결

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,17 +1,15 @@
-import { useEffect } from "react";
 import { useGetOneArticle } from "../request/boardRequest";
 import { Params, useParams } from "react-router-dom";
-import { useQueryClient } from "@tanstack/react-query";
+
+import "../styles/pages/ArticlePage.css";
 
 export default function ArticlePage() {
   const params = useParams<Params<string>>();
   const { data } = useGetOneArticle(params);
 
-  useEffect(() => {}, [params]);
-
   return (
-    <div>
-      <div className="text-white">{data?.content}</div>
+    <div className="article_container">
+      <div className="article_box">{data?.content}</div>
     </div>
   );
 }
